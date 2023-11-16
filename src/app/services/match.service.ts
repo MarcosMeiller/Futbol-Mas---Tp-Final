edc1c4752c96bfe4abd6b2073cb48f70d912fe11
MarcosMeiller <mar.quiton@hotmail.com>
2023-11-16 18:49:05 -0300

    change api key

--- src/app/services/match.service.ts
+++ src/app/services/match.service.ts
@@ -11,7 +11,7 @@ import { Injectable } from '@angular/core';
 export class MatchService {
 
   private url= 'https://v3.football.api-sports.io/fixtures'
-  private  APIKEY='4aae6e0ae3bfc754b192636ab49e3c77'
+  private  APIKEY='71e9962cf38604017c974f8be33f449b'
 
   
   options={headers:new HttpHeaders({
